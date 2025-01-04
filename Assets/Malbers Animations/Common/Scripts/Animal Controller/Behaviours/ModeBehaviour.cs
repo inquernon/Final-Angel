a7522e894973f8a40e5d6ff68b42fa2567576f83
inquernon <inquernon@gmail.com>
2025-01-04 02:22:45 -0500

    Inicializar proyecto de Unity

---
+++ Assets/Malbers Animations/Common/Scripts/Animal Controller/Behaviours/ModeBehaviour.cs
@@ -0,0 +1,168 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace MalbersAnimations.Controller
+{
+
+    [HelpURL("https://malbersanimations.gitbook.io/animal-controller/main-components/manimal-controller/modes#mode-behaviour")]
+    [AddComponentMenu("Malbers/Mode Behavior")]
+    public class ModeBehaviour : StateMachineBehaviour
+    {
+        public ModeID ModeID;
+
+        [Tooltip("Calls 'Animation Tag Enter' on the Modes")]
+        public bool EnterMode = true;
+        [Tooltip("Calls 'Animation Tag Exit' on the Modes")]
+        public bool ExitMode = true;
+
+        [Tooltip("Next Ability to do on the Mode.If is set to -1, The Exit On Ability Logic will be ignored.\n" +
+            "Used this when you need an ability to finish on another Ability.\n" +
+            "E.g. If the wolf is in the Ability SIT, and you activate the HOWL; When HOWL finish you can play again SIT right after")]
+        public int ExitAbility = -1;
+
+
+        [Tooltip("(Experimental)\nIf true the Animation will exit automatically after the Exit Time. No need for exit/interrupted transitions")]
+        public bool NoExitTransitions = false;
+
+        private bool DoExit = false;
+
+        [Range(0, 1), Tooltip("Time to Exit the Animation Automatically with no exit transitions")]
+        public float ExitTime = 0.8f;
+
+        private MAnimal animal;
+        private Mode ModeOwner;
+        private Ability ActiveAbility;
+
+        public void InitializeBehaviour(MAnimal animal)
+        {
+            this.animal = animal;
+
+            if (ModeID != null)
+            {
+                ModeOwner = animal.Mode_Get(ModeID);
+            }
+            else
+            {
+                Debug.LogWarning("There's a Mode behaviour without an ID. Please check all your Mode Animations states.");
+                Destroy(this);
+            }
+        }
+
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            DoExit = false;
+            if (animal == null)
+            {
+                animal = animator.GetComponent<MAnimal>();
+                ModeOwner = animal.Mode_Get(ModeID);
+                // Debug.LogWarning($"Animal is was null? ModeOwner null? {ModeOwner == null}");
+            }
+
+
+            if (ModeID == null) { Debug.LogError("Mode behaviour needs an ID"); return; }
+            if (ModeOwner == null) { Debug.LogError($"There's no [{ModeID.name}] mode on your character"); return; }
+
+            ActiveAbility = ModeOwner.ActiveAbility;
+            if (animal.ModeStatus == Int_ID.Loop) return;            //Means is Looping so Skip!!!
+
+            if (EnterMode) ModeOwner.AnimationTagEnter(stateInfo.fullPathHash);
+        }
+
+        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            //Means is Looping to itself So Skip the Exit Mode EXTREMELY IMPORTANT
+            if (animator.GetCurrentAnimatorStateInfo(layerIndex).fullPathHash == stateInfo.fullPathHash) return;
+
+            if (ExitMode) ModeOwner.AnimationTagExit(ActiveAbility, ExitAbility);
+        }
+
+        public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            ModeOwner.OnModeStateMove(stateInfo, animator, layerIndex);
+
+            if (ExitMode && !DoExit && NoExitTransitions && stateInfo.normalizedTime > ExitTime)
+            {
+                DoExit = true;
+
+                if (ActiveAbility != animal.ActiveMode.ActiveAbility)
+                {
+                    Debug.Log("Playing different Ability ..ingore exit");
+                    return;
+                }
+
+                Debug.Log("Automatic Exit");
+                ModeOwner.AnimationTagExit(ActiveAbility, ExitAbility);
+
+                //animal.Mode_Stop();
+                //animal.SetModeStatus(0);
+            }
+        }
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(ModeBehaviour))]
+    public class ModeBehaviourED : Editor
+    {
+        SerializedProperty EnterMode, ExitMode, ModeID, ExitAbility, NoExitTransitions, ExitTime
+            ;
+        Color RequiredColor = new(1, 0.4f, 0.4f, 1);
+
+        void OnEnable()
+        {
+
+            ModeID = serializedObject.FindProperty("ModeID");
+            EnterMode = serializedObject.FindProperty("EnterMode");
+            ExitMode = serializedObject.FindProperty("ExitMode");
+            ExitAbility = serializedObject.FindProperty("ExitAbility");
+            NoExitTransitions = serializedObject.FindProperty("NoExitTransitions");
+            ExitTime = serializedObject.FindProperty("ExitTime");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            using (new GUILayout.VerticalScope(EditorStyles.helpBox))
+            {
+
+
+                using (new GUILayout.HorizontalScope())
+                {
+                    var currentGUIColor = GUI.color;
+                    GUI.color = ModeID.objectReferenceValue == null ? RequiredColor : currentGUIColor;
+                    EditorGUIUtility.labelWidth = 70;
+                    EditorGUILayout.PropertyField(ModeID);
+
+                    var width = 42;
+                    GUI.color = EnterMode.boolValue ? Color.green : currentGUIColor;
+
+                    EnterMode.boolValue = GUILayout.Toggle(EnterMode.boolValue,
+                                       new GUIContent("Enter"), EditorStyles.miniButton, GUILayout.Width(width));
+
+
+                    GUI.color = ExitMode.boolValue ? Color.green : currentGUIColor;
+
+                    ExitMode.boolValue = GUILayout.Toggle(ExitMode.boolValue,
+                                       new GUIContent("Exit"), EditorStyles.miniButton, GUILayout.Width(width));
+
+                    GUI.color = currentGUIColor;
+
+                }
+                EditorGUIUtility.labelWidth = 0;
+
+                if (ExitMode.boolValue)
+                {
+                    EditorGUILayout.PropertyField(ExitAbility);
+                    EditorGUILayout.PropertyField(NoExitTransitions);
+                    if (NoExitTransitions.boolValue)
+                        EditorGUILayout.PropertyField(ExitTime);
+
+                }
+            }
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+#endif
+}
---
+++ Assets/Malbers Animations/Common/Scripts/Damage/MExplosion.cs
@@ -0,0 +1,195 @@
+﻿using MalbersAnimations.Controller;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace MalbersAnimations
+{
+    [DefaultExecutionOrder(1000)]
+    /// <summary> Explosion Logic</summary>
+    [AddComponentMenu("Malbers/Damage/Explosion Force")]
+
+    public class MExplosion : MDamager
+    {
+        [Tooltip("The Explosion will happen on Start ")]
+        public bool ExplodeOnStart;
+        [Tooltip("Value needed for the AddExplosionForce method default = 0 ")]
+        public float upwardsModifier = 0;
+        [Tooltip("Radius of the Explosion")]
+        public float radius = 10;
+        [Tooltip("Life of the explosion, after this time has elapsed the Explosion gameobject will be destroyed ")]
+        public float life = 10f;
+
+        public int ColliderSize = 50;
+
+        public AnimationCurve DamageCurve = new(MTools.DefaultCurveLinearInverse);
+
+
+        [HideInInspector] public int Editor_Tabs1;
+
+
+        private Collider[] colliders;
+
+        void Start()
+        {
+            colliders = new Collider[ColliderSize];
+
+            if (ExplodeOnStart)
+                Explode();
+        }
+
+        public virtual void Explode()
+        {
+            Physics.OverlapSphereNonAlloc(transform.position, radius, colliders, Layer, triggerInteraction);             //Ignore Colliders
+
+            List<GameObject> Real_Roots = new();
+
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                var col = colliders[i];
+
+                if (col == null) return; //no other collider were found    
+
+                if (dontHitOwner && Owner && col.transform.IsChildOf(Owner.transform)) continue;   //Don't hit yourself
+
+                var rb = col.attachedRigidbody;
+
+                GameObject realRoot = col.transform.FindObjectCore().gameObject;       //Get the gameObject on the entering collider
+
+                //Means the Root is not on the real root since its not on the search layer
+                if (realRoot.layer != col.gameObject.layer)
+                    realRoot = MTools.FindRealParentByLayer(col.transform);
+
+                if (!Real_Roots.Contains(realRoot))
+                {
+                    //Debug.Log("realRoot = " + realRoot);
+
+                    //Distance of the collider and the Explosion
+                    var Distance = Vector3.Distance(transform.position, col.bounds.center);
+
+                    var ExplotionRange = DamageCurve.Evaluate(Distance / radius); //Calculate the explostion range 
+
+                    if (rb != null && rb.useGravity)
+                    {
+                        col.attachedRigidbody.AddExplosionForce(Force * ExplotionRange, transform.position, radius, upwardsModifier, forceMode);
+                    }
+
+
+                    Debugging("Apply Explosion", col);
+
+                    Real_Roots.Add(realRoot); //Affect Only One 
+
+
+                    if (statModifier.ID != null)
+                    {
+                        var modif = new StatModifier(statModifier)
+                        {
+                            Value = statModifier.Value * ExplotionRange    //Do Damage depending the distance from the explosion
+                        };
+
+                        TryDamage(col.gameObject, modif);
+                        TryInteract(col.gameObject);
+
+                        ////Use the Damageable comonent instead!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                        //modif.ModifyStat(other.GetComponentInParent<Stats>());
+                    }
+                }
+
+                col = null; //Clear the Collider
+            }
+            // Debug.Log("-----------------------");
+            Destroy(gameObject, life);
+        }
+
+        private void OnDisable()
+        {
+            StopAllCoroutines();
+
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = (Color.red);
+            Gizmos.DrawWireSphere(transform.position, radius);
+        }
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(MExplosion))]
+    [CanEditMultipleObjects]
+    public class MExposionEd : MDamagerEd
+    {
+        SerializedProperty ExplodeOnStart, upwardsModifier, radius, life, Editor_Tabs1, DamageCurve;
+        protected string[] Tabs1 = new string[] { "General", "Damage", "Extras", "Events" };
+
+        private void OnEnable()
+        {
+            FindBaseProperties();
+
+            ExplodeOnStart = serializedObject.FindProperty("ExplodeOnStart");
+
+            upwardsModifier = serializedObject.FindProperty("upwardsModifier");
+            Editor_Tabs1 = serializedObject.FindProperty("Editor_Tabs1");
+            DamageCurve = serializedObject.FindProperty("DamageCurve");
+
+            radius = serializedObject.FindProperty("radius");
+            life = serializedObject.FindProperty("life");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            DrawDescription("Explosion Damager. Damage is reduced if the target is far from the center of the explosion");
+
+            Editor_Tabs1.intValue = GUILayout.Toolbar(Editor_Tabs1.intValue, Tabs1);
+
+            int Selection = Editor_Tabs1.intValue;
+
+            if (Selection == 0) DrawGeneral();
+            else if (Selection == 1) DrawDamage();
+            else if (Selection == 2) DrawExtras();
+            else if (Selection == 3) DrawEvents();
+
+
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        protected override void DrawGeneral(bool drawbox = true)
+        {
+            using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
+            {
+                EditorGUILayout.LabelField("Explosion", EditorStyles.boldLabel);
+                EditorGUILayout.PropertyField(ExplodeOnStart, new GUIContent("On Start"));
+                EditorGUILayout.PropertyField(DamageCurve);
+                EditorGUILayout.PropertyField(radius);
+                EditorGUILayout.PropertyField(life);
+            }
+            base.DrawGeneral(drawbox);
+        }
+
+
+        private void DrawDamage()
+        {
+            DrawStatModifier();
+            DrawCriticalDamage();
+        }
+
+        private void DrawExtras()
+        {
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            {
+                DrawPhysics(false);
+                EditorGUILayout.PropertyField(upwardsModifier);
+            }
+            EditorGUILayout.EndVertical();
+
+            DrawMisc();
+        }
+    }
+#endif
+
+}
